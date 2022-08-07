3604de245d75ebabdc29217a72881a1c64ce6105
eagle37 <84372934+eagle37@users.noreply.github.com>
2022-08-07 13:23:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+"""Made by Eagle[.]#0831 Don't you dare skid this"""
+import discord
+from discord.ext import commands
+client = commands.Bot(command_prefix="$", self_bot=True)
+token = input("Enter Your Account Token => ")
+cd = commands.CooldownMapping.from_cooldown(5, 6, commands.BucketType.user)
+
+
+@client.event
+async def on_message(message):
+  if not message.guild:
+    bucket = cd.get_bucket(message)
+    retry = bucket.update_rate_limit()
+    if retry:
+      await message.channel.send("Blocking you for spamming | Made By Eagle[.]#0831")
+      await message.author.block()
+      print("Blocked", message.author, "For spamming Dms")
+client.run(token, bot=True)
